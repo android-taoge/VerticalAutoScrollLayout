17cdbf4bc89084eb8243e1fc6c8f68e65d74f5a3
xiaotaoge <13547889604@163.com>
2019-12-08 23:11:44 +0800

    sdfsafs

--- app/src/main/java/com/tangtao/myapplication/AutoScrollRecyclerView.java
+++ app/src/main/java/com/tangtao/myapplication/AutoScrollRecyclerView.java
@@ -5,12 +5,23 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
 
 public class AutoScrollRecyclerView extends RecyclerView {
 
-    private int mState;
-    private OnScrollListener mScrollListener;
+
+    private boolean running = true;
+    private boolean canRun = false;
+    private Disposable mAutoTask = null;
+    private int mCurrentPosition = -1;
 
 
     public AutoScrollRecyclerView(@NonNull Context context) {
@@ -23,37 +34,59 @@ public class AutoScrollRecyclerView extends RecyclerView {
 
     public AutoScrollRecyclerView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        mScrollListener = new OnScrollListener() {
-            @Override
-            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
-                super.onScrollStateChanged(recyclerView, newState);
-                mState = newState;
-            }
-        };
-        addOnScrollListener(mScrollListener);
+        startAuto(this);
     }
 
-
-    @Override
-    public boolean onInterceptTouchEvent(MotionEvent e) {
-        //return mState != 0;
-        //return super.onInterceptTouchEvent(e);
-        return mState == 0;
-    }
 
     @Override
     public boolean onTouchEvent(MotionEvent e) {
         switch (e.getAction()) {
             case MotionEvent.ACTION_DOWN:
+                if (running)
+                    stopAuto();
+                break;
+
             case MotionEvent.ACTION_UP:
-                return mState == 0;
-
-            case MotionEvent.ACTION_MOVE:
-                return mState == 0;
-
-            case MotionEvent.ACTION_POINTER_UP:
-                return false;
+            case MotionEvent.ACTION_CANCEL:
+            case MotionEvent.ACTION_OUTSIDE:
+                if (canRun)
+                    startAuto(this);
+                break;
         }
-        return true;
+        return super.onTouchEvent(e);
+    }
+
+
+    private void startAuto(final AutoScrollRecyclerView recyclerView) {
+
+        if (mAutoTask != null) {
+            mAutoTask.isDisposed();
+        }
+
+        running = true;
+        mAutoTask = Observable.interval(1, 2, TimeUnit.SECONDS).observeOn(AndroidSchedulers.mainThread()).subscribe(new Consumer<Long>() {
+
+            @Override
+            public void accept(Long aLong) {
+
+                if (mCurrentPosition == 0) {
+                    mCurrentPosition = aLong.intValue();
+                } else {
+                    mCurrentPosition++;
+                }
+                recyclerView.smoothScrollToPosition(mCurrentPosition);
+            }
+        });
+
+    }
+
+
+    private void stopAuto() {
+        running = false;
+        canRun = true;
+        if (mAutoTask != null) {
+            mAutoTask.dispose();
+            mAutoTask = null;
+        }
     }
 }
--- app/src/main/java/com/tangtao/myapplication/VerticalAutoScrollLayout.java
+++ app/src/main/java/com/tangtao/myapplication/VerticalAutoScrollLayout.java
@@ -2,28 +2,19 @@ package com.tangtao.myapplication;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.graphics.Color;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.util.SparseArray;
 import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.LinearLayout;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
-import io.reactivex.Observable;
-import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Consumer;
 
 public class VerticalAutoScrollLayout extends LinearLayout {
 
@@ -69,7 +60,6 @@ public class VerticalAutoScrollLayout extends LinearLayout {
                 throw new NullPointerException();
             }
             addView(childView, i, layoutParams);
-            Log.e("添加了第", "==" + i + "个view");
         }
     }
 
@@ -90,91 +80,21 @@ public class VerticalAutoScrollLayout extends LinearLayout {
         CustomRecyclerviewAdapter adapter = new CustomRecyclerviewAdapter(mContext);
         adapter.setDatas(mLists.get(position));
         recyclerView.setAdapter(adapter);
-        //recyclerView.start();
-        final Disposable task = startAuto(recyclerView);
-       // tasks.put(position, task);
-       /* recyclerView.setOnTouchListener(new OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        if (tasks.keyAt(position) == position) {
-                            Disposable cancelTask = tasks.valueAt(position);
-                            cancelTask.dispose();
-                            cancelTask = null;
-                            tasks.removeAt(position);
-                        }
-                        return true;
-
-                    case MotionEvent.ACTION_UP:
-                        Disposable startTask = startAuto(recyclerView);
-                        tasks.put(position, startTask);
-                        return true;
-                }
-                return false;
-            }
-
-        });*/
         return recyclerView;
-    }
-
-    private Disposable startAuto(final AutoScrollRecyclerView recyclerView) {
-        Disposable mAutoTask = null;
-
-        final int[] mCurrentPosition = {-1};
-
-        if (mAutoTask != null) {
-            mAutoTask.isDisposed();
-        }
-
-        mAutoTask = Observable.interval(1, 2, TimeUnit.SECONDS).observeOn(AndroidSchedulers.mainThread()).subscribe(new Consumer<Long>() {
-
-            @Override
-            public void accept(Long aLong) {
-                Log.e("wizardev", "accept: " + aLong.intValue());
-                if (mCurrentPosition[0] == 0) {
-                    mCurrentPosition[0] = aLong.intValue();
-                } else {
-                    mCurrentPosition[0]++;
-                }
-                recyclerView.smoothScrollToPosition(mCurrentPosition[0]);
-            }
-        });
-        return mAutoTask;
     }
 
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
+
         switch (ev.getAction()) {
-            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_DOWN | MotionEvent.ACTION_UP:
                 return false;
-            case MotionEvent.ACTION_MOVE:   //表示父类需要
-                return true;
-            case MotionEvent.ACTION_UP:
-                return false;
-            default:
-                break;
         }
-        return false;    //如果设置拦截，除了down,其他都是父类处理
+        return false;
     }
 
 
-   /* @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                Log.d("TAG", "You down layout");
-
-                break;
-            case MotionEvent.ACTION_UP:
-                Log.d("TAG", "You up layout");
-                break;
-            case MotionEvent.ACTION_MOVE:
-                Log.d("TAG", "You move layout");
-        }
-        return true;
-    }*/
 
 
 }
